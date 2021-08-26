f27dadb6407ee0656c75b49fa29e08b79becec66
Cyrielle Deceneux <cyrielle.deceneux@gmail.com>
2021-08-26 11:05:13 +0200

    ajout fichiers X et y utilisables

--- author_style/pipeline.py
+++ author_style/pipeline.py
@@ -13,28 +13,31 @@ df = features(df)
 print(df.columns)
 
 #selection de X et y dans le dataframe df
-X = df[['preprocess_data','unique_word_count', 'sentences_count', 'stopwords_count','vocab richness']]
+X = df[[
+    'preprocess_data', 'unique_word', 'word_ratio', 'sentences_ratio',
+    'stopwords_ratio', 'vocab richness'
+]]
 y = df["author"]
 
-# Encode categorical variables
-cat_transformer = LabelEncoder()
-y = cat_transformer.fit_transform(y)
+# # Encode categorical variables
+# cat_transformer = LabelEncoder()
+# y = cat_transformer.fit_transform(y)
 
-# transform X features
-column_trans = ColumnTransformer(
-    [('vec', TfidfVectorizer(), 'preprocess_data')], remainder='passthrough')
+# # transform X features
+# column_trans = ColumnTransformer(
+#     [('vec', TfidfVectorizer(), 'preprocess_data')], remainder='passthrough')
 
-X_combined = column_trans.fit_transform(X[[
-    'preprocess_data', 'unique_word_count', 'sentences_count',
-    'stopwords_count', 'vocab richness'
-]])
+# X_combined = column_trans.fit_transform(X[[
+#     'preprocess_data', 'unique_word', 'word_ratio', 'sentences_ratio',
+#     'stopwords_ratio', 'vocab richness'
+# ]])
 
-#split date
-X_train, X_test, y_train, y_test = train_test_split(X_combined, y, test_size=0.30, random_state=42)
+# #split date
+# X_train, X_test, y_train, y_test = train_test_split(X_combined, y, test_size=0.30, random_state=42)
 
-#model
+# #model
 
-nb_model = MultinomialNB()
-model_trained = nb_model.fit(X_train, y_train)
+# nb_model = MultinomialNB()
+# model_trained = nb_model.fit(X_train, y_train)
 
-print(model_trained.score(X_test, y_test))
+# print(model_trained.score(X_test, y_test))
