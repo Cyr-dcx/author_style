7511125a5cb164ecbc9a6d94883a80add27ca992
Jerome Bellon <jerome.bellon@edu.escp.eu>
2021-09-01 16:31:24 +0200

    Streamlit

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import os
+import requests
+
 
 image_header = Image.open('Author_style.png')
 st.header('''Projet Style d'un auteur''')
@@ -21,40 +23,27 @@ length = st.write('Nombre de caractères dans le paragraphe:', len(text_input))
 prediction = st.button('Prédire')
 
 if prediction == True:
-    auteurs = {'BASTIDE Francois-Regis': 0.02,
-    'Albert Camus': 0.0,
-    'Louis-Ferdinand Celine': 0.67,
-    'COHEN Albert': 0.0,
-    'DEBEAUVOIR Simone': 0.0,
-    'Marguerite Duras': 0.23,
-    'Jean Echenoz': 0.0,
-    'Romain Gary': 0.06,
-    'Pierre Guth': 0.0,
-    'Jean Giono': 0.0,
-    'Paul Guth': 0.0,
-    'Joseph Joffo': 0.01,
-    'Joseph Kessel': 0.0,
-    'Patrick Modiano': 0.0,
-    'Georges Perec': 0.0,
-    'QUEFFELEC Henri': 0.0,
-    'QUEFFELEC Yann': 0.0,
-    'Françoise Sagan': 0.0,
-    'Nathalie Sarraute': 0.0,
-    'SCHOENDOERFFER Pierre': 0.0,
-    'Boris Vian': 0.01,
-    'YOURCENAR Marguerite': 0.0}
+    text_input = '%20'.join(text_input.split(" "))
+    response = requests.get(
+        f'https://apiamd64-ywkwoqtfqq-ew.a.run.app/predict?paragraph={text_input}',
+    ).json()
+    auteurs = response['prediction']
+    keys = list(auteurs.keys())
+    values = list(auteurs.values())
 
+    index_max = values.index(max(values))
+    max_auteur = keys[index_max]
 
 
     root_path = os.path.dirname(__file__)
     path_folder = os.path.join(root_path, 'Auteurs_photos')
     images = [image for image in os.listdir(path_folder)]
+    max = max(auteurs.values())
 
-    imag1 = Image.open(os.path.join(path_folder, images[0]))
-    st.image(imag1, width=200)
+    imag1 = Image.open(os.path.join(path_folder, ''.join([max_auteur,'.png'])))
+    st.image(imag1, width=400)
 
-    keys = list(auteurs.keys())
-    values = list(auteurs.values())
+
 
     fig = plt.figure(figsize=(5, 5))
 
@@ -64,6 +53,9 @@ if prediction == True:
     plt.ylabel("Nom des auteurs")
     plt.title("Prédiction du style de l'auteur")
     st.pyplot(fig)
+
+
+
 
 #st.header("""C'est Simone de Beauvoire""")
 #image = Image.open('Simone_de_Beauvoir.png')
