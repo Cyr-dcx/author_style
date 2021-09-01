e8f53672dfd324a779b61d9730b08b4b3481404c
Jerome Bellon <jerome.bellon@edu.escp.eu>
2021-09-01 15:03:44 +0200

    updated api

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from typing import ValuesView
 import streamlit as st
 from PIL import Image
 import matplotlib.pyplot as plt
@@ -20,7 +21,6 @@ length = st.write('Nombre de caractères dans le paragraphe:', len(text_input))
 prediction = st.button('Prédire')
 
 if prediction == True:
-
     auteurs = {'BASTIDE Francois-Regis': 0.02,
     'Albert Camus': 0.0,
     'Louis-Ferdinand Celine': 0.67,
@@ -43,8 +43,8 @@ if prediction == True:
     'SCHOENDOERFFER Pierre': 0.0,
     'Boris Vian': 0.01,
     'YOURCENAR Marguerite': 0.0}
-    
-    
+
+
 
     root_path = os.path.dirname(__file__)
     path_folder = os.path.join(root_path, 'Auteurs_photos')
@@ -53,12 +53,17 @@ if prediction == True:
     imag1 = Image.open(os.path.join(path_folder, images[0]))
     st.image(imag1, width=200)
 
+    keys = list(auteurs.keys())
+    values = list(auteurs.values())
 
-#   keys = auteurs.keys()
-#  values = auteurs.values()
+    fig = plt.figure(figsize=(5, 5))
 
-# st.bar_chart(data=auteurs)
+    plt.barh(keys, values, color='skyblue')
 
+    plt.xlabel("Probabilité que le texte saisi appartienne à cet auteur")
+    plt.ylabel("Nom des auteurs")
+    plt.title("Prédiction du style de l'auteur")
+    st.pyplot(fig)
 
 #st.header("""C'est Simone de Beauvoire""")
 #image = Image.open('Simone_de_Beauvoir.png')
